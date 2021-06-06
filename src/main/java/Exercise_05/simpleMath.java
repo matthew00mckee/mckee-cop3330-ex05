eccb08f4977edb3ee6a79d2c32f07ee4eb9cb46b
matthew00mckee <67074056+matthew00mckee@users.noreply.github.com>
2021-06-06 15:08:47 -0400

    Update and VCS

--- src/main/java/Exercise_05/simpleMath.java
+++ src/main/java/Exercise_05/simpleMath.java
@@ -12,17 +12,19 @@ public class simpleMath {
         System.out.print("What is the first number? ");
         Scanner input = new Scanner(System.in);
         String first = input.next();
-
+        // this changes a string to an integer
         int firstNum = Integer.parseInt(first);
         System.out.print("What is the second number? ");
         String second = input.next();
         int secondNum = Integer.parseInt(second);
 
+        //code for the math
         int sum = firstNum + secondNum;
         int subtract = firstNum-secondNum;
         int multiply = firstNum * secondNum;
         int divide = firstNum/secondNum;
 
+        //one printout but I entered it out a bit for readability
         System.out.print(firstNum + " + "+ secondNum + " = "+ sum + "\n"
         +firstNum + " - "+secondNum+" = "+subtract+"\n"
         +firstNum+" * "+secondNum+" = "+ multiply+"\n"
